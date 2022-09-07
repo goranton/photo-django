28bdfa2ea674abefd1da4e71e3c3d970e8aaec87
Anton Gorbunov <goranton98@gmail.com>
2022-09-08 00:17:53 +0300

    use database settings from OS env

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'photo',
-        'USER': 'local',
-        'PASSWORD': 'local',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'database',
         'PORT': 5432,
     }
